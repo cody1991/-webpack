82f4d0cb8b15d3df1ef9932b5f064601f2ca1882
cody1991 <codytang@tencent.com>
2021-04-24 22:50:27 +0800

    demo template

--- demo-template/webpack.config.js
+++ demo-template/webpack.config.js
@@ -1,3 +1,9 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
